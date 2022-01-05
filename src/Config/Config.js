8b5e2a3b643add1bddaf5d82e2bf1f335f39f003
hrishikesh <hshinde166@gmail.com>
2022-01-06 00:09:55 +0530

    firebase config file for email and password authentication

---
+++ src/Config/Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5bs0EAeQOrLmdfy7ZLGx8ZutUVIKdL14",
+    authDomain: "anime-facts-74bd7.firebaseapp.com",
+    projectId: "anime-facts-74bd7",
+    storageBucket: "anime-facts-74bd7.appspot.com",
+    messagingSenderId: "1034677620527",
+    appId: "1:1034677620527:web:b61500a06e1ef3086b1bda",
+    measurementId: "G-1J81J4JH8E"
+  };
+
+  export default firebaseConfig;

1e74b79127393758052cf9801689fb1d055fff8d
hrishikesh <hshinde166@gmail.com>
2022-01-06 00:11:54 +0530

    Footer for the application

---
+++ src/Layout/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="bg-dark text-white text-muted text-center p-3 fixed-bottom">
+            Hrishi 2022
+        </footer>
+      );
+}
+
+export default Footer;

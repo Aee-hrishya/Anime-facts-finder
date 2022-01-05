d6dfa6b7b2662a9be8e2bdfe6b328b0a9bcdccbc
hrishikesh <hshinde166@gmail.com>
2022-01-06 00:12:44 +0530

    Created the different routes for the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import PageNotFound from "./Pages/PageNotFound";
+import NavBar from "./Layout/NavBar";
+import Footer from "./Layout/Footer";
+import UserContext from "./Context/UserContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import './App.css';
 
+//Firebase
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./Config/Config";
+
+//Init firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
+
+  const [user, setUser] = useState(null);//Initial state of the application
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <ToastContainer />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </UserContext.Provider>
   );
 }
 
